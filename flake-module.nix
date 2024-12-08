76f784ac4a5fa3449465cd1a7afdc07b1835e5c8
tarc <tarcisio@gmail.com>
2024-12-08 18:16:43 -0300

    Refactor

---
+++ flake-module.nix
@@ -0,0 +1,36 @@
+{
+  inputs,
+  withSystem,
+  ...
+}:
+{ lib, flake-parts-lib, ... }:
+let
+  inherit (flake-parts-lib) mkPerSystemOption;
+  inherit (lib) mkAliasDefinitions mkOption types;
+in
+{
+  imports = [
+  ];
+  options = {
+    perSystem = mkPerSystemOption (
+      { config, pkgs, system, options, ... }:
+      {
+        options.ardutoolchains = mkOption {
+          description = ''
+            Setup ArduPilot toolchains with flake-parts.
+          '';
+
+          type = types.lazyAttrsOf (
+            types.submoduleWith { modules = import ./modules/modules.nix { inherit pkgs lib withSystem system; }; }
+          );
+
+          default = { };
+        };
+
+        config = {
+          packages = lib.mapAttrs (_name: ardutoolchain: pkgs.hello) config.ardutoolchains;
+        };
+      }
+    );
+  };
+}
